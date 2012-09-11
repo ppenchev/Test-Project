87df8ae1509807324fdb39422e9229f2feb45228
Plamen Penchev <plamen.penchev@bedegaming.com>
2012-09-11 22:03:40 +0100

    Commit WCF web service

--- SGP.Components.Notifications/NotificationService/INotificationService.cs
+++ SGP.Components.Notifications/NotificationService/INotificationService.cs
@@ -1,11 +1,14 @@
 ﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+using NotificationService.Model;
 
 namespace NotificationService
 {
     [ServiceContract]
     public interface INotificationService
     {
-        //[OperationContract]
-
+        [OperationContract]
+        [WebGet(UriTemplate = "/message", ResponseFormat = WebMessageFormat.Json)]
+        Message GetMessages();
     }
 }
